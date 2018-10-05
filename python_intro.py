954c9ebc5f1705a2b5688565c21ed3e42c99e46d
leyorgar <leyre_ortiz@hotmail.com>
2018-10-05 11:12:21 +0200

    My first Django app

---
+++ python_intro.py
@@ -0,0 +1,33 @@
+print('Hola!')
+
+if 3 > 2: 
+    print('Falso!') 
+else:
+    print('Verdadero')
+
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! :(")
+
+def  hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi('Ola')
+
+for i in range(1,6):
+    print(i)
